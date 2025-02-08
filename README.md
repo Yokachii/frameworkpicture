f550c6c2996afd6ff6bfa9771b1067ff5e0ff48c
Yokachii <149374407+Yokachii@users.noreply.github.com>
2025-02-08 15:32:15 +0100

    push readme & image

--- README.md
+++ README.md
@@ -1,28 +1,3 @@
 # Framework pictures
 
-site web pour framework picture
-## Installation
-
-Pour commencer, clone mon repo
-
-```bash
-    git clone https://github.com/Milo-star/elliott_website
-    cd elliott_website
-    npm install 
-    npm start
-```
-
-
-
-
-## Deploiement sur Vercel
-
-Après chaque modification voici les étapes à suivre.
-
-```bash
-    push sur la branch DEVELOP
-    faire une PR sur la branch MAIN
-```
-
-
-
+site web pour framework pictures
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,22 +16,22 @@ const About = ({refs}) => {
 
                 <div className={styles.image}>
                     <div className={styles.image_container}>
-                        <img className={styles.img} src='/img/EVENT.webp' alt='ilustration'></img>
+                        <img className={styles.img} src='/img/EVENT.jpg' alt='ilustration'></img>
 
                         <div className={styles.text}><h2>Event</h2><p>Film after movie </p></div>
                     </div>
                     <div className={styles.image_container}>
-                        <img className={styles.img} src='/img/REPORTAGE.webp' alt='ilustration'></img>
+                        <img className={styles.img} src='/img/EVENT.jpg' alt='ilustration'></img>
 
                         <div className={styles.text}><h2>Reportage</h2><p>Documentaire, pub</p></div>
                     </div>
                     <div className={styles.image_container}>
-                        <img className={styles.img} src='/img/SOCIAL-MEDIA.webp' alt='ilustration'></img>
+                        <img className={styles.img} src='/img/EVENT.jpg' alt='ilustration'></img>
 
                         <div className={styles.text}><h2>Social Media</h2><p>Création de contenu,..</p></div>
                     </div>
                     <div className={styles.image_container}>
-                        <img className={styles.img} src='/img/FILM-CORPORATE.webp' alt='ilustration'></img>
+                        <img className={styles.img} src='/img/EVENT.jpg' alt='ilustration'></img>
 
                         <div className={styles.text}><h2>Film Coroprate</h2><p>interview, shooting photo,..</p></div>
                     </div>
